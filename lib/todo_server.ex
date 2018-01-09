16ee46864448954fd8168748603ef4f4e3ff738f
Leonardo Tegon <ltegon93@gmail.com>
2018-01-08 23:45:54 -0200

    Add `update_entry` and `delete_entry` functions

--- lib/todo_server.ex
+++ lib/todo_server.ex
@@ -16,6 +16,14 @@ defmodule TodoServer do
     end
   end
 
+  def update_entry(todo_server, new_entry) do
+    send(todo_server, {:update_entry, new_entry})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    send(todo_server, {:delete_entry, entry_id})
+  end
+
   defp loop(todo_list) do
     new_todo_list = receive do
       message -> process_message(todo_list, message)
@@ -30,5 +38,14 @@ defmodule TodoServer do
 
   defp process_message(todo_list, {:entries, caller, date}) do
     send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+    todo_list
+  end
+
+  defp process_message(todo_list, {:update_entry, new_entry}) do
+    TodoList.update_entry(todo_list, new_entry)
+  end
+
+  defp process_message(todo_list, {:delete_entry, entry_id}) do
+    TodoList.delete_entry(todo_list, entry_id)
   end
 end

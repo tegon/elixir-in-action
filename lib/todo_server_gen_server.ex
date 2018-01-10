1bcb8fb0e1d32593a505dc634977c0ea3d4a56ba
Leonardo Tegon <ltegon93@gmail.com>
2018-01-10 00:32:51 -0200

    Add TodoServerGenServer

---
+++ lib/todo_server_gen_server.ex
@@ -0,0 +1,43 @@
+defmodule TodoServerGenServer do
+  use GenServer
+
+  def init(_) do
+    {:ok, TodoList.new}
+  end
+
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    {:noreply, TodoList.add_entry(todo_list, new_entry)}
+  end
+
+  def handle_cast({:update_entry, new_entry}, todo_list) do
+    {:noreply, TodoList.update_entry(todo_list, new_entry)}
+  end
+
+  def handle_cast({:delete_entry, entry_id}, todo_list) do
+    {:noreply, TodoList.delete_entry(todo_list, entry_id)}
+  end
+
+  def handle_call({:entries, date}, _, todo_list) do
+    {:reply, TodoList.entries(todo_list, date), todo_list}
+  end
+
+  def start do
+    GenServer.start(TodoServerGenServer, nil)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def update_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:update_entry, new_entry})
+  end
+
+  def delete_entry(todo_server, entry_id) do
+    GenServer.cast(todo_server, {:delete_entry, entry_id})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+end

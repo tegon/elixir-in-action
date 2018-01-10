4ecf620875703afa4b5892d79df90a94e01e9d81
Leonardo Tegon <ltegon93@gmail.com>
2018-01-10 00:25:50 -0200

    Add KeyValueStoreGenServer

---
+++ lib/key_value_store_gen_server.ex
@@ -0,0 +1,35 @@
+defmodule KeyValueStoreGenServer do
+  use GenServer
+
+  def init(_) do
+    :timer.send_interval(5000, :cleanup)
+    {:ok, %{}}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  def handle_info(:cleanup, state) do
+    IO.puts "performing cleanup..."
+    {:noreply, state}
+  end
+
+  def handle_info(_, state), do: {:noreply, state}
+
+  def start do
+    GenServer.start(KeyValueStoreGenServer, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+end
